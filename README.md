66e0e9196f2520c210addb9b7cc56d2f3154a625
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2023-05-20 16:10:09 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Refferences
+- [Remote Code Execution System just like kirat said](https://blog.devgenius.io/case-study-remote-code-execution-engine-system-63aa43344f24)
+- [Docker Engine API to create and work with containers](https://docs.docker.com/engine/api/v1.42/#tag/Container/operation/ContainerCreate)
+- [An Implementation of the Docker API](https://github.com/apocas/dockerode)
+- [Running Docker/Commnds using "exec" module in node js](https://stackoverflow.com/questions/35644155/how-can-i-dynamically-create-a-docker-container-from-a-node-application)
+- [Using RabbitMQ in node js app](https://www.rabbitmq.com/tutorials/tutorial-one-javascript.html)
+- [Parallel Processing/Multithreading in node js](https://deepsource.com/blog/nodejs-worker-threads/)
+- __Video Tutorials__
+    - [Docker Tutorial, all about docker](https://youtu.be/3c-iBn73dDE)
+    - [What are Messagin queue, RabbitMQ with nodjs](https://youtu.be/e03c3CIGtYU)
+    - [Scale node js app using cluster module](https://youtu.be/9RLeLngtQ3A)
---
+++ server/index.js
@@ -0,0 +1,232 @@
+const express = require('express')
+const app = express()
+const port = 3001
+const cors = require("cors");
+const docker = new require("dockerode")();
+const amqp = require("amqplib");
+const { v4: uuidv4 } = require('uuid');
+const cluster = require("cluster");
+
+
+
+
+/// ------------- MIDDLEWARES ------------------
+
+app.use(express.json());
+app.use(cors());
+
+
+
+
+/// ------------------ RCP SERVER(WORKER) -------------------
+
+async function startRcpServerAsWorker() {
+  console.log("statingRcpServer")
+  //connect to rabbitmq
+  const { connection, channel } = await connectRabbitMQ();
+
+  // declare rcpcodeexecutionqueue
+  channel.prefetch(1);        // limit the no. of unack msg, fetch only one msg
+  channel.assertQueue(codeExecutionQueue);
+
+  // consumer for rcpCodeExecutionQueue
+  channel.consume(codeExecutionQueue, async msg => {
+
+    const { code, language } = JSON.parse(msg.content.toString());
+
+    try {
+        // process the rcp request / execute code inside container
+        const executionResult = await executeUserCodeInContainer(code, language);
+
+        // publish the result to the response queue
+        channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(executionResult)), {
+            correlationId: msg.properties.correlationId
+        });
+
+        // acknowledge the message (dqueue)
+        channel.ack(msg);
+    }
+    catch (err) {
+        console.log("Error processing RCP request for execution: ", err);
+        process.exit(1);    // terminate
+    }
+
+  });
+
+}
+
+
+
+
+/// --------------- RPC CLIENT ----------------
+
+const codeExecutionQueue = "rpcCodeExecutionQueue";
+
+// Connect to rabbit mq
+async function connectRabbitMQ() {
+
+  const connection = await amqp.connect("amqp://localhost:5672");
+  const channel = await connection.createChannel();
+  return{ connection, channel};
+
+}
+
+// Publish message to queue
+async function publishMessageToCodeExecutionQueue(connection, channel, code, language, res) {
+
+  // set up responsequeue (temp)
+  const responseQueue = await channel.assertQueue("", { exclusive: true });
+  const responseQueueName = responseQueue.queue;
+
+  const correlationId = uuidv4();
+
+  // consumer for the response queue
+  channel.consume(responseQueueName, msg => {
+      if (msg.properties.correlationId == correlationId) {
+          const result = JSON.parse(msg.content.toString());
+          res.status(200).json(result.result);
+          console.log(result.result);
+          channel.close();
+          connection.close();
+      }
+  }, { noAck: true });
+
+  // publish msg(code) to rcpcodeExecutionQueue / send rcp request
+  channel.sendToQueue(codeExecutionQueue, Buffer.from(JSON.stringify({ code, language })), {
+      correlationId,
+      replyTo: responseQueueName
+  });
+
+}
+
+
+
+
+/// ----------------- DOCKER LOGIC ----------------------
+
+// Create container according to user selected language
+async function createDockerContainer(language, code) {
+
+  const containerConfig = {
+    Image: getDockerImage(language), //node
+    Cmd: getExecutionCommand(language, code), // ["node", "-e", code]
+    Tty: true,
+  }
+  // same as docker create --image imageName --tty --command cmdToRun
+  const container = await docker.createContainer(containerConfig) 
+  
+  return container;
+
+}
+
+// helper fun to get image according to user Selected language
+function getDockerImage(language) {
+
+  let image;
+  image = "node";
+  return image;
+
+}
+
+// helper fun to get Execution command acc... to user selected language
+function getExecutionCommand(language, code){
+
+  let cmd;
+  switch (language) {
+    case 'cpp':
+      cmd = ["g++", "-o", code, "-x", "c++", "-"];
+      break;
+    case "js":
+      cmd = ["node", "-e", code];
+      break;
+    default:
+      throw new Error(`unsupprted language: ${language}`);
+  }
+
+  return cmd;
+
+}
+
+
+
+
+/// ------------------- CODE EXECUTION LOGIC -----------------
+
+async function executeUserCodeInContainer(code, language) {
+  return new Promise(async (resolve, reject) => {
+    console.log("executing code");
+    const container = await createDockerContainer(language, code);
+    await container.start();
+    const containerExitStatus = await container.wait(); // wait for container to exit
+    await container.start();    // start container
+
+    // get logs
+    const logs = await container.logs({ stdout: true, stderr: true });
+
+    // return output/error
+    if (containerExitStatus.StatusCode === 0) {
+      resolve({ result: logs.toString() });
+    } else {
+      resolve({ result: logs.toString() });
+    }
+  });
+}
+
+
+
+
+///----------------- ROUTES -----------------------
+
+app.post("/submissions", async (req, res) => {
+  let body = req.body;
+  let userSubmittedCode = body.submission.code;
+  let codeLanguage = body.submission.language;
+
+  try {
+
+    const { connection, channel } = await connectRabbitMQ();
+    await publishMessageToCodeExecutionQueue(connection, channel, userSubmittedCode, codeLanguage, res);
+    
+  }
+  catch (er) {
+      console.log("Error Publishing message to RcpCodeExecution queue", er);
+      res.status(500).json({ err: "Somethig Went Wrong" });
+  }
+
+});
+
+
+
+
+/// ------------- SCALE USING CLUSTER ----------------
+
+// if (cluster.isMaster) {
+//   for (let i = 0; i < 2; i++) {
+//     cluster.fork();
+//   }
+
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} exited`);
+//     cluster.fork();
+//   });
+// }
+// else {
+  startRcpServerAsWorker();
+// }
+
+
+
+
+// --------- Start Express Server ----------
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+server.on('error', (error) => {
+  if (error.code === 'EADDRINUSE') {
+    console.log(`Port ${port} is already in use`);
+  } else {
+    console.error('An error occurred:', error);
+  }
+});
